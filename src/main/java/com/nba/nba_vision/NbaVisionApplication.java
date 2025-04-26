e44e8a8e30df21640f207371cb3d1874256132b8
Tallal Mohar <tallalmoohar@gmail.com>
2025-04-26 15:42:55 -0700

    database set up done - need to fill database with data

---
+++ src/main/java/com/nba/nba_vision/NbaVisionApplication.java
@@ -0,0 +1,13 @@
+package com.nba.nba_vision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaVisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nba/nba_vision/NbaVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nba.nba_vision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
