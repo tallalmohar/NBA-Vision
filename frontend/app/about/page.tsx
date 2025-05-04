87bee6d53249164c88c92e0beb62bfea1439614b
Tallal Mohar <tallalmoohar@gmail.com>
2025-05-03 18:22:30 -0700

    need to add props

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -2,7 +2,10 @@ import DropDown from "../components/DropDown";
 
 export default function Page() {
     return (
-        <main className={"flex justify-center mt-[20px] h-full"}>
+        <main className={"grid gap-3 justify-center mt-[20px] h-full"}>
+            <DropDown />
+            <DropDown />
+            <DropDown />
             <DropDown />
         </main>
     );
--- frontend/app/components/DropDown.tsx
+++ frontend/app/components/DropDown.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { useState } from "react";
 
+
 export default function DropDown() {
     const [open, setOpen] = useState(false);
 
     return (
-        <div className="w-[970px] h-[37px]">
+        <div className="w-[970px] ">
             <div
                 onClick={() => setOpen(!open)}
                 className={`cursor-pointer p-4  border-1 border-yellow-500 rounded-t shadow transition ${open ? 'bg-yellow-500' : 'bg-white'}`}
@@ -14,19 +15,21 @@ export default function DropDown() {
             </div>
             {/* Collapsed content below */}
             {open && (
-                <div className=" p-4 bg-white border-1 border-yellow-500 rounded-b">
-                    <h5>Roaster Size</h5>
-                    <p>
+                <div className="p-4 bg-white border-1 border-yellow-500 rounded-b">
+
+                    <h5 className={"text-md font-bold"}>Roaster Size</h5>
+                    <p className="mt-5px">
                         To join the game select a fantasy basketball roster of 10 players, consisting of:
                     </p>
-                    <ul>
+                    <ul className="list-disc pl-6">
                         <li>5 Back Court players</li>
                         <li>5 Front Court players</li>
                     </ul>
-                    <h5>Players Per Team</h5>
+                    <h5 className={"text-md font-bold"}>Players Per Team</h5>
                     <p>
-                        To join the game select a fantasy basketball roster of 10 players, consisting of:
+                        You can select up to 2 players from a single NBA team.
                     </p>
+
                 </div>
             )}
         </div>
