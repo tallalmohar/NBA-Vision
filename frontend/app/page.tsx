afbac5ba097843ebe0d035a00978e21c8d0d1005
Tallal Mohar <tallalmoohar@gmail.com>
2025-05-15 10:46:22 -0700

    Need to work on home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 export default function Home() {
     return (
-        <main className="p-8">
-            <h1 className="text-3xl font-bold text-blue-900">Welcome to Home Page</h1>
-            <p className="mt-2 text-gray-700">This is a simple page component using Tailwind CSS.</p>
+        <main className="p-8 flex flex-col min-h-screen">
+            <div>
+
+            </div>
         </main>
     );
 }
