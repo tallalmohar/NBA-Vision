a91e34b7b6ac608da600e24a1128e4b10c3d85c4
Tallal Mohar <tallalmoohar@gmail.com>
2025-05-16 13:34:51 -0700

    working on home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,11 @@
 'use client';
+import Image from "next/image";
+
 export default function Home() {
     return (
         <main className="p-8 flex flex-col min-h-screen">
-            <div>
-
+            <div className="flex justify-center items-center border ">
+                <Image src="/homePage.png" alt="Goats of the game" width="1180" height="225" ></Image>
             </div>
         </main>
     );
