09571fcec864992cd152bbec7aed4b7de574ee56
Tallal Mohar <tallalmoohar@gmail.com>
2025-05-16 15:37:40 -0700

    some more QOL features

--- frontend/app/components/SidewayDiv.tsx
+++ frontend/app/components/SidewayDiv.tsx
@@ -5,7 +5,7 @@ export default function SidewayDiv({ title , paragraph, noleftBorder }:{title: s
 
         <div className={`flex flex-col justify-center  w-75 border-b-0 border-t-0 ${noleftBorder ?'border-l-0': ''} border-r-0 border-2 border-gray-400`}>
             <h2 className="text-xl font-bold pl-2">{title}</h2>
-            <p className="pl-2">{paragraph}</p>
+            <p className="pl-2 pt-2">{paragraph}</p>
         </div>
 
     )
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,11 +10,15 @@ export default function Home() {
             <div className="flex justify-center items-center">
                 <Image src="/homePage.png" alt="Goats of the game" width="1180" height="225" ></Image>
             </div>
-            <div className={"flex flex-row justify-center items-center mt-4"}>
+            <div className={"flex flex-row justify-center items-center mt-10"}>
                 <SidewayDiv title="BUILD YOUR ROSTER" paragraph="Pick your 10 player NBA roster using your $100m budget." noleftBorder={true}/>
                 <SidewayDiv title="CREATE LEAGUES" paragraph="Create leagues and invite your friends to compete throughout the season." noleftBorder={false}/>
                 <SidewayDiv title="SET YOUR LINE-UPS" paragraph="Choose your line-up for the next gameday and watch your starting 5." noleftBorder={false}/>
                 <SidewayDiv title="SIGN AND DROP PLAYERS" paragraph="Sign and waive players through the season to improve your team. " noleftBorder={false}/>
+            </div>
+
+            <div className={"flex justify-center mt-20"}>
+                <h1 className="text-4xl font-bold">HOW TO PLAY</h1>
             </div>
         </main>
     );
